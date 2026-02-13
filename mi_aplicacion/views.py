e3f58d9699c7cda29c98238d893fe9cf00204d60
nicollesandoval <nicollesandovalmaeb@gmail.com>
2026-02-12 23:32:27 -0400

    Delete mi_aplicacion directory

--- mi_aplicacion/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Actividad
-
-def lista_actividades(request):
-    actividades = Actividad.objects.all()
-    return render(request, 'mi_aplicacion/lista_actividades.html', {'actividades': actividades})
